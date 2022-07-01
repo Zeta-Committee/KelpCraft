0a06bffdcda373ec25b83258e52f1f9a24c2ae5c
Goss Chinese <108513113+GossChinese@users.noreply.github.com>
2022-07-01 22:32:13 +0800

    tools!

---
+++ src/main/java/committee/zeta/kelpcraft/goss/common/tier/KelpTier.java
@@ -0,0 +1,64 @@
+package committee.zeta.kelpcraft.goss.common.tier;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.util.LazyValue;
+
+import java.util.function.Supplier;
+
+/**
+ * @author GossChinese
+ */
+
+public enum KelpTier implements IItemTier {
+    KELP(0, 50, 2.5F, 0.1F, 15, () -> {
+        return Ingredient.of(ItemTags.PLANKS);
+    });
+
+    private final int level;
+    private final int uses;
+    private final float speed;
+    private final float damage;
+    private final int enchantmentValue;
+    private final LazyValue<Ingredient> repairIngredient;
+
+    private KelpTier(int level, int uses, float speed, float damage, int enchantmentValue, Supplier<Ingredient> repairIngredient) {
+        this.level = level;
+        this.uses = uses;
+        this.speed = speed;
+        this.damage = damage;
+        this.enchantmentValue = enchantmentValue;
+        this.repairIngredient = new LazyValue<>(repairIngredient);
+    }
+
+    @Override
+    public int getUses() {
+        return this.uses;
+    }
+
+    @Override
+    public float getSpeed() {
+        return this.speed;
+    }
+
+    @Override
+    public float getAttackDamageBonus() {
+        return this.damage;
+    }
+
+    @Override
+    public int getLevel() {
+        return this.level;
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return this.enchantmentValue;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+}
--- src/main/java/committee/zeta/kelpcraft/goss/init/ItemInit.java
+++ src/main/java/committee/zeta/kelpcraft/goss/init/ItemInit.java
@@ -1,8 +1,8 @@
 package committee.zeta.kelpcraft.goss.init;
 
 import committee.zeta.kelpcraft.goss.KelpCraft;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
+import committee.zeta.kelpcraft.goss.common.tier.KelpTier;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,4 +15,11 @@ public class ItemInit {
 
     public static final RegistryObject<Item> KELP_BLOCK_ITEM = ITEMS.register("kelp_block", ()-> new BlockItem(BlockInit.KELP_BLOCK.get(), new Item.Properties()));
     public static final RegistryObject<Item> KELP_CRAFTING_TABLE_BLOCK_ITEM = ITEMS.register("kelp_crafting_table", ()-> new BlockItem(BlockInit.KELP_CRAFTING_TABLE_BLOCK.get(), new Item.Properties()));
+
+    public static final RegistryObject<Item> KELP_SWORD = ITEMS.register("kelp_sword", ()-> new SwordItem(KelpTier.KELP, 3, -2.4F, (new Item.Properties()).tab(ItemGroup.TAB_COMBAT)));
+    public static final RegistryObject<Item> KELP_SHOVEL = ITEMS.register("kelp_shovel", ()-> new ShovelItem(KelpTier.KELP, 1.5F, -3.0F, (new Item.Properties()).tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item> KELP_PICKAXE = ITEMS.register("kelp_pickaxe", ()-> new PickaxeItem(KelpTier.KELP, 1, -2.8F, (new Item.Properties()).tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item> KELP_AXE = ITEMS.register("kelp_axe", ()-> new AxeItem(KelpTier.KELP, 6.0F, -3.2F, (new Item.Properties()).tab(ItemGroup.TAB_TOOLS)));
+    public static final RegistryObject<Item> KELP_HOE = ITEMS.register("kelp_hoe", ()-> new HoeItem(KelpTier.KELP, 0, -3.0F, (new Item.Properties()).tab(ItemGroup.TAB_TOOLS)));
+
 }
